2e16ee4cc9366b2be8f4f2b8e2e86451d30e5c8e
Dominik <hi@domthedev.com>
2021-10-13 14:51:06 +0200

    add header and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductListComponent} from "./components/product-list/product-list.component";
+import {CartComponent} from "./components/cart/cart.component";
+import {ConfirmationComponent} from "./components/confirmation/confirmation.component";
+import {ProductItemDetailComponent} from "./components/product-item-detail/product-item-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'confirmation', component: ConfirmationComponent },
+  { path: 'product-detail', component: ProductItemDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
