1292c02e2f34c2185b899c207b3634d932d42f2e
Dominik <hi@domthedev.com>
2021-10-13 14:40:41 +0200

    initial commit

---
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-item-detail',
+  templateUrl: './product-item-detail.component.html',
+  styleUrls: ['./product-item-detail.component.css']
+})
+export class ProductItemDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
